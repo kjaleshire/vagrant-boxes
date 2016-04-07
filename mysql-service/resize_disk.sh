89b552222efb0648eaeb6b9c3ca6e576a02370ac
Kyle Aleshire <kjaleshire@gmail.com>
2016-04-07 12:36:45 -0500

    Surround shell variables with quotes

--- mysql-service/resize_disk.sh
+++ mysql-service/resize_disk.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 VM_DIRECTORY="$HOME/VMs"
+echo resize_disk mysql-service: Detaching current disk...
 vboxmanage storageattach mysql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium none
-vboxmanage clonehd --format VDI $VM_DIRECTORY/mysql-service/box-disk1.vmdk $VM_DIRECTORY/mysql-service/box-disk1.vdi
-vboxmanage modifyhd $VM_DIRECTORY/mysql-service/box-disk1.vdi --resize 122880
-vboxmanage storageattach mysql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium $VM_DIRECTORY/mysql-service/box-disk1.vdi
-vboxmanage closemedium $VM_DIRECTORY/mysql-service/box-disk1.vmdk --delete
+echo resize_disk mysql-service: Cloning current disk...
+vboxmanage clonehd --format VDI "$VM_DIRECTORY/mysql-service/box-disk1.vmdk" "$VM_DIRECTORY/mysql-service/box-disk1.vdi"
+echo resize_disk mysql-service: Upsizing new disk...
+vboxmanage modifyhd "$VM_DIRECTORY/mysql-service/box-disk1.vdi" --resize 122880
+echo resize_disk mysql-service: Attaching new disk...
+vboxmanage storageattach mysql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium "$VM_DIRECTORY/mysql-service/box-disk1.vdi"
+echo resize_disk mysql-service: Deleting old disk...
+vboxmanage closemedium "$VM_DIRECTORY/mysql-service/box-disk1.vmdk" --delete
--- postgresql-service/resize_disk.sh
+++ postgresql-service/resize_disk.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 VM_DIRECTORY="$HOME/VMs"
+echo resize_disk postgresql-service: Detaching current disk...
 vboxmanage storageattach postgresql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium none
-vboxmanage clonehd --format VDI $VM_DIRECTORY/postgresql-service/box-disk1.vmdk $VM_DIRECTORY/postgresql-service/box-disk1.vdi
-vboxmanage modifyhd $VM_DIRECTORY/postgresql-service/box-disk1.vdi --resize 122880
-vboxmanage storageattach postgresql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium $VM_DIRECTORY/postgresql-service/box-disk1.vdi
-vboxmanage closemedium $VM_DIRECTORY/postgresql-service/box-disk1.vmdk --delete
+echo resize_disk postgresql-service: Cloning current disk...
+vboxmanage clonehd --format VDI "$VM_DIRECTORY/postgresql-service/box-disk1.vmdk" "$VM_DIRECTORY/postgresql-service/box-disk1.vdi"
+echo resize_disk postgresql-service: Upsizing new disk...
+vboxmanage modifyhd "$VM_DIRECTORY/postgresql-service/box-disk1.vdi" --resize 122880
+echo resize_disk postgresql-service: Attaching new disk...
+vboxmanage storageattach postgresql-service --storagectl 'SATAController' --port 0 --device 0 --type hdd --medium "$VM_DIRECTORY/postgresql-service/box-disk1.vdi"
+echo resize_disk postgresql-service: Deleting old disk...
+vboxmanage closemedium "$VM_DIRECTORY/postgresql-service/box-disk1.vmdk" --delete
