e44e3464ab25d208933816ab1604bdd610f5d89f
Kyle Aleshire <kjaleshire@gmail.com>
2016-07-06 13:11:22 -0500

    Linux 2.6 distro (ubuntu-lucid)

---
+++ ubuntu-lucid64/vagrant_bootstrap.sh
@@ -0,0 +1,26 @@
+# Some parts borrowed from https://github.com/rails/rails-dev-box/
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    # Use noninteractive to avoid dialogs (eg mysql-server asking for password)
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install "$@"
+}
+
+# Make sure the system is using UTF-8
+locale-gen en_US.UTF-8
+export LC_CTYPE="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+echo ">>>> updating package information"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+install utilities zerofree git-core make
+
+# cleanup
+echo ">>>> cleaning up"
+sudo apt-get clean
