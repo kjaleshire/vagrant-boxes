bff7def0578d163766afdad34506f4252e3e5e3d
Kyle Aleshire <kjaleshire@gmail.com>
2017-04-10 13:09:39 -0500

    Don't continue resizing after the first failure

--- mysql-service/resize_disk.sh
+++ mysql-service/resize_disk.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 VM_DIRECTORY="$HOME/VMs"
-echo resize_disk mysql-service: Detaching current disk...
-vboxmanage storageattach mysql-service --storagectl 'SCSI' --port 0 --type hdd --medium none
-echo resize_disk mysql-service: Cloning current disk...
-vboxmanage clonehd --format VDI "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vmdk" "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi"
-echo resize_disk mysql-service: Upsizing new disk...
-vboxmanage modifyhd "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi" --resize 122880
-echo resize_disk mysql-service: Attaching new disk...
-vboxmanage storageattach mysql-service --storagectl 'SCSI' --port 0 --type hdd --medium "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi"
-echo resize_disk mysql-service: Deleting old disk...
+echo resize_disk mysql-service: Detaching current disk... &&
+vboxmanage storageattach mysql-service --storagectl 'SCSI' --port 0 --type hdd --medium none &&
+echo resize_disk mysql-service: Cloning current disk... &&
+vboxmanage clonehd --format VDI "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vmdk" "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi" &&
+echo resize_disk mysql-service: Upsizing new disk... &&
+vboxmanage modifyhd "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi" --resize 122880 &&
+echo resize_disk mysql-service: Attaching new disk... &&
+vboxmanage storageattach mysql-service --storagectl 'SCSI' --port 0 --type hdd --medium "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vdi" &&
+echo resize_disk mysql-service: Deleting old disk... &&
 vboxmanage closemedium "$VM_DIRECTORY/mysql-service/ubuntu-xenial-16.04-cloudimg.vmdk" --delete
