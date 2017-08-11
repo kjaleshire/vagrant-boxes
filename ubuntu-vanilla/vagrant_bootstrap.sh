17a339d635ead3daf622424e454d6820f907848a
Kyle Aleshire <kjaleshire@gmail.com>
2017-08-11 13:47:24 -0500

    Upgrade the box to zesty

--- ubuntu-vanilla/vagrant_bootstrap.sh
+++ ubuntu-vanilla/vagrant_bootstrap.sh
@@ -13,10 +13,10 @@ function install {
 #
 # need the following bootstrap code to make sure the hostname
 # is in the hosts file:
-if ! grep -q $(cat /etc/hostname) /etc/hosts; then
-    sudo sh -c "echo >> /etc/hosts"
-    sudo sh -c "echo 127.0.0.1 $(cat /etc/hostname) >> /etc/hosts"
-fi
+# if ! grep -q $(cat /etc/hostname) /etc/hosts; then
+#     sudo sh -c "echo >> /etc/hosts"
+#     sudo sh -c "echo 127.0.0.1 $(cat /etc/hostname) >> /etc/hosts"
+# fi
 
 # Make sure the system is using UTF-8
 locale-gen en_US.UTF-8
@@ -30,21 +30,20 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 install utilities zerofree xorriso qemu nasm git make gdb
-install "patched gdb stuff" texinfo flex bison python-dev ncurses-dev
 
-echo ">>>> installing rust toolchain"
-curl https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-setup -o rustup-setup -sSf
-chmod u+x rustup-setup
-./rustup-setup -y
-rm rustup-setup
-echo "PATH=\$PATH:\$HOME/.cargo/bin" >> $HOME/.profile
-source $HOME/.profile
-rustup update stable
-rustup update nightly
-rustup default nightly
-rustup target add arm-unknown-linux-gnueabihf
-rustup target add armv7-unknown-linux-gnueabihf
-rustup target add aarch64-unknown-linux-gnu
+# echo ">>>> installing rust toolchain"
+# curl https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-setup -o rustup-setup -sSf
+# chmod u+x rustup-setup
+# ./rustup-setup -y
+# rm rustup-setup
+# echo "PATH=\$PATH:\$HOME/.cargo/bin" >> $HOME/.profile
+# source $HOME/.profile
+# rustup update stable
+# rustup update nightly
+# rustup default nightly
+# rustup target add arm-unknown-linux-gnueabihf
+# rustup target add armv7-unknown-linux-gnueabihf
+# rustup target add aarch64-unknown-linux-gnu
 
 # cleanup
 echo ">>>> cleaning up"
